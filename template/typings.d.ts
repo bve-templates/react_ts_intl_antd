466b42adf0f2f30f0e80cbfaa6d8eaac47693087
施华军 <shihj@yuntron.org>
2022-02-11 11:06:12 +0800

    add

--- template/typings.d.ts
+++ template/typings.d.ts
@@ -15,41 +15,4 @@ declare module '*.svg' {
   export default url;
 }
 
-declare interface Window {
-  ethereum: any;
-  __WEB3_CHAIN__CONFIG__: {
-    addEthereumChain: {
-      chainId: string;
-      chainName: string;
-      rpcUrls: string[];
-      blockExplorerUrls: string[];
-      nativeCurrency: {
-        name: string;
-        symbol: string;
-        decimals: number;
-      };
-    };
-    name: string;
-    // 目录/src/assets/svgs
-    icon: string;
-    ExchangeProxy: string;
-    NFT: string;
-    TransferProxy: string;
-    Farm: string;
-    defaultTokenIndex: number;
-    tokens: {
-      [key: string]: {
-        symbol: string;
-        decimals: number;
-        toFixed: number;
-        address: string;
-        approveAmount: string;
-        type?: number;
-        assetType: number;
-      };
-    };
-    decimals: {
-      LAND: number;
-    };
-  };
-}
+declare interface Window {}
